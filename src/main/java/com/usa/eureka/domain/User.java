20286fe61dfb6f8b250e030d2ec653a0c6b7749a
Alex-CHO <hyungjin98@gmail.com>
2024-12-10 16:26:56 +0900

    added class and config to test kafka

--- src/main/java/com/usa/eureka/domain/User.java
+++ src/main/java/com/usa/eureka/domain/User.java
@@ -1,10 +1,37 @@
 package com.usa.eureka.domain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name=users)
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+    // 추후 implements UserDetails 추가
+    // TODO Setter DB 구축 후 삭제 예정
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String email;
+
+    private String nickname;
+
+    private Long deposit;
+
+    @Builder
+    public User(String email, String nickname, Long deposit) {
+        this.email = email;
+        this.nickname = nickname;
+        this.deposit = deposit;
+    }
+
+
+    //TODO Kafka 먼저 구현 후 테스트 하고, Repository 나 Entity(Domain) 구현은 DB 구현할때 같이해도 늦지 않는다.
+    //TODO 그래서 일단 대충 하고, service 함수 하나 구현하여 kafka 테스트 하고, 그때 기능에 대해서 고민하자.
+
+
 }
--- src/main/java/service/kafkaConsumer.java
+++ src/main/java/service/kafkaConsumer.java
@@ -1,9 +1,43 @@
 package service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.usa.eureka.domain.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Slf4j
 public class kafkaConsumer {
+
+    @KafkaListener(topics = "order-user-topic")
+    public void updateDeposit(String kafkaMessage) {
+        log.info("Kafka Message: -> {}", kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {
+            });
+        } catch (JsonProcessingException jsonProcessingException) {
+            jsonProcessingException.printStackTrace();
+        }
+        // DB 아직 구축 안됨
+        // DB에서 읽어온걸로 치고
+        String email = (String) map.get("email");
+        User user = new User(email, "alex", 10000000L);
+
+        user.setDeposit(user.getDeposit() - (Long) map.get("amount"));
+
+        log.info("User email: -> {}", user.getEmail());
+        log.info("User nickname: -> {}", user.getNickname());
+        log.info("User deposit: -> {}", user.getDeposit());
+
+
+    }
 }
