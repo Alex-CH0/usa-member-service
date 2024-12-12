c284a78d6ac67750491958e4c86373e8bc60142e
Alex-CH0 <hyungjin98@gmail.com>
2024-12-12 22:59:12 +0900

    changed name member -> user

--- src/main/java/com/usa/eureka/controller/UserController.java
+++ src/main/java/com/usa/eureka/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.usa.eureka.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MemberController {
+public class UserController {
 
     @GetMapping("/test")
     public String test() {
